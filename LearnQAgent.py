1d2b2bf451b8229c6168b24bf0aabe610abb29ee
abbgabcal <54662435+abbgabcal@users.noreply.github.com>
2020-05-11 12:51:57 +0200

    Kommentarer

--- LearnQAgent.py
+++ LearnQAgent.py
@@ -1,53 +1,51 @@
-import gym                      #Nödvändiga imports av bibliotek. 
+import gym                                                                                                          #Nödvändiga imports av bibliotek. 
 from random import randint
 import numpy as np
 import pandas as pd
 
-env = gym.make('gym_mouse.envs:mouse-v0')   #Ladda in miljön.  
+env = gym.make('gym_mouse.envs:mouse-v0')                                                                           #Ladda in miljön.  
 
-Q = np.zeros([env.observation_space.n, env.action_space.n]) #Skapar ett Q-table
+Q = np.zeros([env.observation_space.n, env.action_space.n])                                                         #Skapar ett Q-table
 
-gamma = 0.7             #Gamma (greed) värde mellan 0 och 1 som bestämmer hur mycket nutida belöningar ska värderas kontra framtida belöningar. 
-alpha = 0.5             #Alpha (learning rate) värde mellan 0 och 1 som bestämmer hur fort AI:n ska lära sig. Fortare inte alltid bättre
-epsilon = 0.8           #Epsilon (random) värde mellan 0 och 1 som bestämmer hur ofta ett slumpmässigt val ska göras för att potentiellt lära sig mera eller hitta ett bättre alternativ. 
-epsilon_decay = 0.99    #Epsilon_decay värde mellan 0 och 1 som bestämmer hur mycket epsilon ska minskas över tid. 
+gamma = 0.7                                                                                                         #Gamma (greed) värde mellan 0 och 1 som bestämmer hur mycket nutida belöningar ska värderas kontra framtida belöningar. 
+alpha = 0.5                                                                                                         #Alpha (learning rate) värde mellan 0 och 1 som bestämmer hur fort AI:n ska lära sig. Fortare inte alltid bättre
+epsilon = 0.8                                                                                                       #Epsilon (random) värde mellan 0 och 1 som bestämmer hur ofta ett slumpmässigt val ska göras för att potentiellt lära sig mera eller hitta ett bättre alternativ. 
+epsilon_decay = 0.99                                                                                                #Epsilon_decay värde mellan 0 och 1 som bestämmer hur mycket epsilon ska minskas över tid. 
 
-episodes = 5000        #Antal gånger som AI:n ska spela spelet
+episodes = 5000                                                                                                     #Antal gånger som AI:n ska spela spelet
 
-done = False            #Variabel so mhåller koll på ifall spelet är över. 
-cheesearray = []        #Samling av antalet insamlade ostar för varje omgång för att kunna göra jämförelser. 
-rewardarray = []        #Samling av belöningar från varje omgång för att kunna göra jämförelser. 
+done = False                                                                                                        #Variabel so mhåller koll på ifall spelet är över. 
+cheesearray = []                                                                                                    #Samling av antalet insamlade ostar för varje omgång för att kunna göra jämförelser. 
+rewardarray = []                                                                                                    #Samling av belöningar från varje omgång för att kunna göra jämförelser. 
 
 
 for episode in range(episodes): 
-    reward = 0                          #Återställ belöningen till 0.
-    epsilon = epsilon*epsilon_decay     #Räkna ut det nya värdet på epsilon.
+    reward = 0                                                                                                      #Återställ belöningen till 0.
+    epsilon = epsilon*epsilon_decay                                                                                 #Räkna ut det nya värdet på epsilon.
 
-    state, done = env.reset()           #Startar om miljön och tar in de första värdena. 
+    state, done = env.reset()                                                                                       #Startar om miljön och tar in de första värdena. 
     while not done:
-        if np.random.rand() < epsilon:          #Kollar om en slumpmässig action ska genomföras
-            action = env.action_space.sample()  #Väljer ett slumpmässigt nummer av 0, 1, 2 eller 3 som action. 
+        if np.random.rand() < epsilon:                                                                              #Kollar om en slumpmässig action ska genomföras
+            action = env.action_space.sample()                                                                      #Väljer ett slumpmässigt nummer av 0, 1, 2 eller 3 som action. 
         else: 
-            action = np.argmax(Q[state])        #Väljer den action som borde ge bäst resultat från Q-tablet
-        next_state, reward, done, info = env.step(action)   #Utför vald action och sparar resultatet i variablerna. 
-        Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[next_state]) - Q[state, action]) #Uppdaterar Q-tablet utifrån de nya värdena, de gamla värdena samt hyperparametrarna med hjälp av Bellmanformeln. 
-        state = next_state  #Uppdaterar state
-    rewardarray.append(reward)  #Sparar ner belöningsresultatet. 
-    cheesearray.append(info)    #Sparar ner antalet insamlade ostar. 
+            action = np.argmax(Q[state])                                                                            #Väljer den action som borde ge bäst resultat från Q-tablet
+        next_state, reward, done, info = env.step(action)                                                           #Utför vald action och sparar resultatet i variablerna. 
+        Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[next_state]) - Q[state, action])   #Uppdaterar Q-tablet utifrån de nya värdena, de gamla värdena samt hyperparametrarna med hjälp av Bellmanformeln. 
+        state = next_state                                                                                          #Uppdaterar state
+    rewardarray.append(reward)                                                                                      #Sparar ner belöningsresultatet. 
+    cheesearray.append(info)                                                                                        #Sparar ner antalet insamlade ostar. 
     print(cheesearray)
     print(episode)
-    if episode % 500 == 0:      #If sats som utför var 500:e episod. Sparar ner Q-tablet till ett excelblad så det går att använda senare. 
+    if episode % 500 == 0:                                                                                          #If sats som utför var 500:e episod. Sparar ner Q-tablet till ett excelblad så det går att använda senare. 
         df = pd.DataFrame(Q)
         filepath = r"C:\Users\s9gabcal\OneDrive - ABB Industrigymnasium\Teknik\Code\Python\AI-lek\Musen\Excel.xls"
         df.to_excel(f"{episode}_episodes.xlsx")
 
-df = pd.DataFrame(Q)        #Sparar ner det slutgiltiga resultatet i ett excelblad. 
+df = pd.DataFrame(Q)                                                                                                #Sparar ner det slutgiltiga resultatet i ett excelblad. 
 df.to_excel(f"{episodes}_episodes.xlsx")
 
 print(f"Average cheese eaten: {np.mean(cheesearray)}")
 print(f"Average reward: {np.mean(rewardarray)}")
-
-
 
 env.render()
 
--- gym-mouse/gym_mouse/envs/mouse_env.py
+++ gym-mouse/gym_mouse/envs/mouse_env.py
@@ -1,4 +1,4 @@
-import gym                  #imports av nödvändiga bibliotek. 
+import gym                                                  #imports av nödvändiga bibliotek. 
 from gym import spaces
 import time
 from random import randint
@@ -6,10 +6,10 @@ from random import randint
 from tkinter import *
 master = Tk()
 
-class MouseEnv(gym.Env):                        #Skapa klassen som innehåller hela miljön. 
+class MouseEnv(gym.Env):                                    #Skapa klassen som innehåller hela miljön. 
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):                         #Funktion som körs igång när klassen startar. I gym.make() i Q-learningagenten
+    def __init__(self):                                     #Funktion som körs igång när klassen startar. I gym.make() i Q-learningagenten
         
         self.action_space = spaces.Discrete(4)              #Action space. Antalet olika "actions" som AI:n kan göra. Upp, ner, höger och vänster. 
         self.observation_space = spaces.Discrete(1100)      #Antal observationer som AI:n kan göra. Hundra musplaceringar för varje möjlig ostplacering (11*100). 
@@ -22,7 +22,7 @@ class MouseEnv(gym.Env):                        #Skapa klassen som innehåller h
         self.cheeseamount=1                                 #Antal ostar på planen, går att ändra men då krävs det att observation_space ändras. 
         self.currentcheeses=0                               #Antal ostar på planen just nu. 
         self.cheeseplaces=[[3,3],[2,1],[4,5],[7,3],[2,8],[9,1],[5,7],[8,3],[4,1],[2,7],[5,4]]   #Alla olika placeringar som en ost kan skapas på. 
-        self.speed= 1/30                                     #Uppdaterngshastighet, för inlärning rekommenderas 0 (snabbast möjliga), för testning 0.2 (möjligt att se vad som händer). 
+        self.speed= 1/30                                    #Uppdaterngshastighet, för inlärning rekommenderas 0 (snabbast möjliga), för testning 0.2 (möjligt att se vad som händer). 
 
         self.w = 10     #Planens bredd.
         self.h = 10     #Planens höjd.
@@ -35,13 +35,13 @@ class MouseEnv(gym.Env):                        #Skapa klassen som innehåller h
         self.sizevalue=50
         for row in range(0,self.w): 
             for col in range(0,self.h):
-                coord = self.distanceX, self.distanceY, self.distanceX+self.sizevalue, self.distanceY+self.sizevalue #y,x, w, h
+                coord = self.distanceX, self.distanceY, self.distanceX+self.sizevalue, self.distanceY+self.sizevalue 
                 self.distanceX+=self.sizevalue
                 self.C.create_rectangle(coord, fill="white")
             self.distanceY+=self.sizevalue
             self.distanceX=0
 
-        self.text = Text(master, height=2, width=30)    #Skapar textrutan under planen. 
+        self.text = Text(master, height=2, width=30)                    #Skapar textrutan under planen. 
         self.text.pack()
 
         self.Mouse=self.C.create_rectangle(self.getcoord(self.mouse[0],self.mouse[1]), fill="brown")    #Skapar musen på Getcoord hämtar koordinaterna för musen position. 
@@ -86,17 +86,17 @@ class MouseEnv(gym.Env):                        #Skapa klassen som innehåller h
             self.reward += 10
     
     def step(self, action):                                             #Funktionen som kommunicerar med AI:n. 
-        self.takeAction(action)     #Kör takeAction() och skickar in den action som AI:n sa skulle användas. 
+        self.takeAction(action)                                         #Kör takeAction() och skickar in den action som AI:n sa skulle användas. 
 
-        mouse_pos = self.mouse[0] + self.mouse[1]*10    #Skriver ut musens position som ett tal mellan 0 och 100. Tiotalet motsvarar y-koordinaten i 2d-arrayen och entalet motsvarar x-koordinaten. 
-        cheese_pos = self.cheeseplaces.index([self.cheeseX, self.cheeseY])  #Tar den nuvarande ostens position genom att hitta dens index i cheeseplaces arrayen. 
-        observation = cheese_pos*100 + mouse_pos            #Skapar en observation som AI:n kan använda. Hundratalet motsvarar ostens position, tiotalet musens y-koordinat och entalet musens x-koordniat 
+        mouse_pos = self.mouse[0] + self.mouse[1]*10                                 #Skriver ut musens position som ett tal mellan 0 och 100. Tiotalet motsvarar y-koordinaten i 2d-arrayen och entalet motsvarar x-koordinaten. 
+        cheese_pos = self.cheeseplaces.index([self.cheeseX, self.cheeseY])           #Tar den nuvarande ostens position genom att hitta dens index i cheeseplaces arrayen. 
+        observation = cheese_pos*100 + mouse_pos                                     #Skapar en observation som AI:n kan använda. Hundratalet motsvarar ostens position, tiotalet musens y-koordinat och entalet musens x-koordniat 
 
-        return observation, self.reward, self.episode_over, self.cheeseeaten    #Skickar iväg all information till AI:n
+        return observation, self.reward, self.episode_over, self.cheeseeaten         #Skickar iväg all information till AI:n
 
-    def takeAction(self, action):  #Funktion som styr musen utifrån vad AI:n skickar in för action. 
+    def takeAction(self, action):       #Funktion som styr musen utifrån vad AI:n skickar in för action. 
         time.sleep(self.speed)
-        steps = [0,0]   #Variabel som håller koll på hur musen förflyttar sig. 
+        steps = [0,0]                   #Variabel som håller koll på hur musen förflyttar sig. 
         if action == 0:
             if self.mouse[1] > 0:       #Kollar om musen är för nära väggen för att gå åt det hållet. Är musen för nära kan den inte gå men förlorar ändå poäng och energi för att AI:n skalära sig att inte gå in i väggen. 
                 self.mouse[1] = self.mouse[1]-1 #Uppåt, Förflyttar musens position i 2d-arrayen. 
@@ -152,12 +152,12 @@ class MouseEnv(gym.Env):                        #Skapa klassen som innehåller h
         self.currentcheeses=0
 
         self.C.coords(self.Mouse, self.getcoord(self.mouse[0],self.mouse[1])) #Förflyttar musen till den ursprungliga positionen
-        self.NewCheese()    #Skapar den första osten
+        self.NewCheese()                                                      #Skapar den första osten
 
         print("Env reset")
         
-        mouse_pos = self.mouse[0] + self.mouse[1]*10        #Skriver ut musens position som ett tal mellan 0 och 100. Tiotalet motsvarar y-koordinaten i 2d-arrayen och entalet motsvarar x-koordinaten. 
+        mouse_pos = self.mouse[0] + self.mouse[1]*10                         #Skriver ut musens position som ett tal mellan 0 och 100. Tiotalet motsvarar y-koordinaten i 2d-arrayen och entalet motsvarar x-koordinaten. 
         cheese_pos = self.cheeseplaces.index([self.cheeseX, self.cheeseY])   #Tar den nuvarande ostens position genom att hitta dens index i cheeseplaces arrayen.
-        observation = cheese_pos*100 + mouse_pos     #Skapar en observation som AI:n kan använda. Hundratalet motsvarar ostens position, tiotalet musens y-koordinat och entalet musens x-koordniat 
+        observation = cheese_pos*100 + mouse_pos                             #Skapar en observation som AI:n kan använda. Hundratalet motsvarar ostens position, tiotalet musens y-koordinat och entalet musens x-koordniat 
 
-        return observation, self.episode_over   #Skickar iväg all ny information till AI:n
+        return observation, self.episode_over                                #Skickar iväg all ny information till AI:n
